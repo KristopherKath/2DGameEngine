6105bd0fdc107186c7b530db068a368972c5144a
Kris <kristopher.kath@gmail.com>
2020-10-22 13:05:34 -0700

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
+	}
+	else
+	{
+		std::cout << "SDL initialization succeeded!";
+	}
+
+	std::cin.get();
+	return 0;
+}
