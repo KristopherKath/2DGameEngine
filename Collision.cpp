142e2d4063c453e897a26adb60f7ee730bcb2b6a
Kris <kristopher.kath@gmail.com>
2020-10-24 16:44:54 -0700

    Update Collision.cpp

--- Collision.cpp
+++ Collision.cpp
@@ -3,6 +3,7 @@
 //Checks if two rectangles are overlapping with one another 
 bool Collision::CheckRectangleCollision(const SDL_Rect& rectangleA, const SDL_Rect& rectangleB)
 {
+    //Check if the two rectangles overlap on the x and y axis
     return (
         rectangleA.x + rectangleA.w >= rectangleB.x &&
         rectangleB.x + rectangleB.w >= rectangleA.x &&
